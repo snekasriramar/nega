3c5beb974d859b84148915c4d4feee0bad866111
snekasriramar <45815719+snekasriramar@users.noreply.github.com>
2019-01-25 15:34:32 +0530

    Update largest number.py

--- largest number.py
+++ largest number.py
@@ -1,6 +1,6 @@
 # Python program to find the largest number among the three input numbers
 
-# change the values of num1, num2 and num3
+# change the  elements of num1, num2 and num3
 # for a different result
 num1 = 11
 num2 = 15
