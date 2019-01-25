1aa8e7f62d2a10ff852cae629bc85c66088254da
snekasriramar <45815719+snekasriramar@users.noreply.github.com>
2019-01-25 15:02:06 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -1,11 +1,12 @@
 # Python Program - Check Alphabet or Not
 
-print("Enter '0' for exit");
-ch = input("Enter any character ");
+print("Enter '0' for exit.");
+ch = input("Enter any character: ");
 if ch == '0':
     exit();
 else:
-    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z'))
-    	print(ch, "is an alphabet");
+    if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
+    	print(ch, "is an alphabet.");
     else:
     	print(ch, "is not an alphabet.");
+ 
