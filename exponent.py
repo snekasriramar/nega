3497695de2c30efe876272e9325e00b00d08d1ad
snekasriramar <45815719+snekasriramar@users.noreply.github.com>
2019-02-18 15:23:08 +0530

    Update exponent.py

--- exponent.py
+++ exponent.py
@@ -1,4 +1,4 @@
 squares=5
-result=list(map(lambda x:2**x,raange(terms)));
-for i in range(squares);
-print("2 raised to the power of"i,"is" results[i]);
+result=list(map(lambda x: 2** x, range(terms)))
+for i in range(squares):
+print("2 raised to the power of",i,"is" results[i])
