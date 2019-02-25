093ef7dd97a0b2dadc3b2880306c16e6bd234343
snekasriramar <45815719+snekasriramar@users.noreply.github.com>
2019-02-25 14:14:23 +0530

    Create odd numbers.py

---
+++ odd numbers.py
@@ -0,0 +1,6 @@
+firstnum = int(raw_input("enter a number:"))
+secondnum = int(raw_input("enter another number (integer):"))
+print ('first number:'+ str(firstnum))
+print ('second number:' + str(secondnum))
+oddsum = 2 #just a placeholder
+print ('The sum of all odd integers between' + ' ' + str(firstnum) + ' ' + 'and' + ' ' + str(secondnum) + ' ' + 'is' + ' ' + str(oddsum))
